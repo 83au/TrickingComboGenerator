f7c25b2963b6ecd4942072a99b07767fb5a424ea
Beau Sherman <brsherman88@gmail.com>
2020-03-06 20:09:51 -0800

    Add buildTrick function.

--- src/js/index.js
+++ src/js/index.js
@@ -12,30 +12,45 @@ const state = {};
 // =============== FUNCTIONS ==================
 // ============================================
 
-function buildTrick(maxDiff, preTrick) {
+function buildTrick(maxDiff, prevTrick) {
   // 1) Initiate new Trick object
+  const trick = new Trick();
 
   // 2) Generate level using maxDiff
+  trick.generateLevel(maxDiff);
+  console.log(trick.level);
 
   // 3) Generate trick using prevTrick
+  trick.generateTrick(prevTrick);
 
   // 4) Add trick to state's current trick property
+  state.currTrick = trick.trickObj;
 
   // 5) Set name
+  trick.setName();
 
   // 6) Generate transition
+  if (prevTrick) trick.generateTransition(prevTrick.landing);
 
   // 7) Generate landing
+  trick.generateLanding();
 
   // 8) Handle hook
+  trick.handleHook();
 
   // 9) Handle takeoff
+  trick.handleTakeoff();
 
   // 10 Handle landing modifier
+  trick.handleLandingMod();
 
   // 11) Format transition
+  if (trick.transition) trick.transition = Model.formatMod(trick.transition);
 
   // 12) Display trick in UI
+  View.displayTrick(prevTrick, trick);
+
+  console.log(trick.name);
 }
 
 
@@ -44,14 +59,19 @@ function generateCombo(maxDiff, numTricks) {
 
   // 1) Clear combo container and state object's trick properties
   View.clearContainer(View.elements.comboContainer);
-  state.combo = [];
+  state.trickCount = 0;
   state.prevTrick = undefined;
-  state.currentTrick = undefined;
+  state.currTrick = undefined;
 
   // 2) Create loop that builds tricks until combo has numTricks in it
   do {
+    if (state.currTrick) {
+      state.prevTrick = state.currTrick;
+      state.currTrick = undefined;
+    }
     buildTrick(maxDiff, state.prevTrick);
-  } while (state.combo.length < numTricks);
+    state.trickCount += 1;
+  } while (state.trickCount < numTricks);
 }
 
 
