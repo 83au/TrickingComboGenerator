da97ea03e12f91eb1d103b50225661b7974f2adc
Beau Sherman <brsherman88@gmail.com>
2020-03-10 00:26:02 -0700

    Refactor app controller into oop.

--- src/js/index.js
+++ src/js/index.js
@@ -7,90 +7,90 @@ import * as View from './view';
 // TESTING
 // import * as Data from './models/data';
 
-const state = {};
+const app = {
+  state: {},
 
-setEventListeners();
+  init() {
+    this.setEventListeners();
+  },
 
-// =========== EVENT LISTENERS ============
-function setEventListeners() {
-  View.elements.generateComboBtn.addEventListener('click', () => {
-    const choices = View.getChoices();
-    generateCombo(choices.difficulty, choices.numTricks);
-  });
+  setEventListeners() {
+    View.elements.generateComboBtn.addEventListener('click', () => {
+      const choices = View.getChoices();
+      this.generateCombo(choices.difficulty, choices.numTricks);
+    });
 
-  // document.getElementById('randomComboOption').addEventListener('click', () => {
-  //   document.querySelector('.start-screen').style.display = 'none';
-  //   document.querySelector('.randomComboBox').style.display = 'block';
-  // });
+    // document.getElementById('randomComboOption').addEventListener('click', () => {
+    //   document.querySelector('.start-screen').style.display = 'none';
+    //   document.querySelector('.randomComboBox').style.display = 'block';
+    // });
 
-  // document.getElementById('backToChoices').addEventListener('click', () => {
-  //   document.querySelector('.randomComboBox').style.display = 'none';
-  //   document.querySelector('.start-screen').style.display = 'block';
-  // });
-}
+    // document.getElementById('backToChoices').addEventListener('click', () => {
+    //   document.querySelector('.randomComboBox').style.display = 'none';
+    //   document.querySelector('.start-screen').style.display = 'block';
+    // });
+  },
 
+  generateCombo(maxDiff, numTricks) {
+    this.clear();
 
-// =============== FUNCTIONS ==================
-// ============================================
-
-function generateCombo(maxDiff, numTricks) {
-  clear();
-
-  let officialNumTricks;
-  if (numTricks === 'random') {
-    officialNumTricks = Model.random(10) + 1;
-  } else {
-    officialNumTricks = numTricks;
-  }
-
-  let officialMaxDiff;
-  if (maxDiff === 'random') {
-    officialMaxDiff = Model.random(2) + 1;
-  } else {
-    officialMaxDiff = maxDiff;
-  }
-
-  do {
-    if (state.currTrick) {
-      state.prevTrick = state.currTrick;
-      state.currTrick = undefined;
+    let officialNumTricks;
+    if (numTricks === 'random') {
+      officialNumTricks = Model.random(10) + 1;
+    } else {
+      officialNumTricks = numTricks;
     }
 
-    buildTrick(officialMaxDiff, state.prevTrick);
-    state.trickCount += 1;
-  } while (state.trickCount < officialNumTricks);
-}
+    let officialMaxDiff;
+    if (maxDiff === 'random') {
+      officialMaxDiff = Model.random(2) + 1;
+    } else {
+      officialMaxDiff = maxDiff;
+    }
+
+    do {
+      if (this.state.currTrick) {
+        this.state.prevTrick = this.state.currTrick;
+        this.state.currTrick = undefined;
+      }
+
+      this.buildTrick(officialMaxDiff, this.state.prevTrick);
+      this.state.trickCount += 1;
+    } while (this.state.trickCount < officialNumTricks);
+  },
+
+  clear() {
+    console.clear();
+    View.clearContainer(View.elements.comboContainer);
+    this.state.trickCount = 0;
+    this.state.prevTrick = undefined;
+    this.state.currTrick = undefined;
+  },
+
+  buildTrick(maxDiff, prevTrick) {
+    const trick = new Trick();
+
+    trick.generateLevel(maxDiff);
+    trick.generateTrick(prevTrick);
+    trick.setName();
+
+    if (prevTrick) {
+      trick.generateTransition(prevTrick.landing);
+    } else {
+      trick.takeoff = Model.chooseFromList(trick.trickObj.setups, 'takeoffModifiers');
+    }
+
+    trick.generateLanding();
+    trick.handleHook();
+    trick.handleTakeoff();
+    trick.handleLandingMod();
+
+    if (trick.transition) trick.transition = Model.formatMod(trick.transition);
+
+    View.displayTrick(prevTrick, trick);
+    this.state.currTrick = trick;
+  },
+};
 
 
-function clear() {
-  console.clear();
-  View.clearContainer(View.elements.comboContainer);
-  state.trickCount = 0;
-  state.prevTrick = undefined;
-  state.currTrick = undefined;
-}
-
-
-function buildTrick(maxDiff, prevTrick) {
-  const trick = new Trick();
-
-  trick.generateLevel(maxDiff);
-  trick.generateTrick(prevTrick);
-  trick.setName();
-
-  if (prevTrick) {
-    trick.generateTransition(prevTrick.landing);
-  } else {
-    trick.takeoff = Model.chooseFromList(trick.trickObj.setups, 'takeoffModifiers');
-  }
-
-  trick.generateLanding();
-  trick.handleHook();
-  trick.handleTakeoff();
-  trick.handleLandingMod();
-
-  if (trick.transition) trick.transition = Model.formatMod(trick.transition);
-
-  View.displayTrick(prevTrick, trick);
-  state.currTrick = trick;
-}
+app.init();
